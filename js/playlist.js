6001b07b9524cf449f00fdcf49f08f0ef6ed6072
Dzhamshed Shoismatuloev <j.shoismatuloev@gmail.com>
2017-03-03 14:38:11 -0500

    Add files via upload

---
+++ js/playlist.js
@@ -0,0 +1,37 @@
+const PlaylistManager = {};
+
+ 
+PlaylistManager.tracks = [];
+
+
+PlaylistManager.currentSong = 0;
+
+    
+PlaylistManager.addTrack = (track = reqParam()) => {
+        PlaylistManager.tracks.push(track);
+
+}; 
+
+PlaylistManager.removeById = (id) => {
+    for (let i = 0; i < PlaylistManager.tracks.length; i++) {
+        const track = PlaylistManager.tracks[i];
+        if (track.id === id) {
+            PlaylistManager.tracks.splice(i, 1);
+
+            break;
+        }
+    }
+}
+
+
+PlaylistManager.getNextSong = () => {
+    PlaylistManager.currentSong++;
+    const {tracks, currentSong} = PlaylistManager;
+
+    const len = PlaylistManager.tracks.length;
+        if (currentSong === len) {
+            PlaylistManager.currentSong = 0;
+        }
+
+        return tracks[PlaylistManager.currentSong].id;
+}
